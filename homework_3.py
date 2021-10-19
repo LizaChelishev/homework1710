869d630f6184a823a156c3ad9deccb4513899ef1
LizaChelishev <lizachelishev@gmail.com>
2021-10-19 21:47:29 +0300

    done

---
+++ homework_3.py
@@ -0,0 +1,6 @@
+def print_numbers(X = 10):
+    for number in range (1, X+1):
+        print (number)
+
+print_numbers()
+print_numbers(3)
---
+++ homework_4.py
@@ -0,0 +1,21 @@
+import random
+
+
+def game():
+    number_of_guesses = 0
+    lucky_number = random.randint(1, 100)
+    while True:
+        guessed_number = int(input('Guess a number between 1 and 100: '))
+        if guessed_number == lucky_number:
+            print('bingo')
+            break
+        elif guessed_number > lucky_number:
+            print('too high')
+        elif guessed_number < lucky_number:
+            print('too low')
+        number_of_guesses += 1
+    print('Number of guesses is: ', number_of_guesses)
+    return number_of_guesses
+
+
+game()
